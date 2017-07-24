aee2205cae745034afd837fc7d700f91270f1610
Filip Kudła <filip.kudla@snow.dog>
2017-07-24 21:59:25 +0200

    Update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,18 @@ module.exports = {
   head: {
     title: 'Poldon WPA',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { 
+        charset: 'utf-8' 
+      },
+      { 
+        name: 'viewport', 
+        content: 'width=device-width, initial-scale=1' 
+      },
     ]
   },
   build: {
-    vendor: ['axios']
+    vendor: [
+      'axios'
+    ]
   }
 }
