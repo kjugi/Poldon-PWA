eea676692c358c4d237738aa1a6c7ed1e1b4bd7b
Filip Kudła <filip.kudla@snow.dog>
2017-07-26 11:19:18 +0200

    Changes in index.vue

---
+++ pages/index.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="container">
+    <h1>Poldon Posts</h1>
+
+    <div class="settings">
+      <div class="settings__row">
+        <a href="#" class="settings__navigation" @click="setPrevPage()">
+          << Prev
+        </a>
+
+        <span class="settings__text">
+          Page: {{ $store.state.page }}
+          of
+          {{ actualTotalPages }}
+        </span>
+
+        <a href="#" class="settings__navigation" @click="setNextPage()">
+          Next >>
+        </a>
+
+        <span class="settings__text">
+          Per page: {{ $store.state.postsPerPage }}
+        </span>
+      </div>
+
+      <ul class="list settings__row">
+        <li v-for="variant in variantsPostsPerPage" class="list__item">
+          <a href="#" class="list__link" @click="changeVariantPerPage">
+            {{ variant }}
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="posts">
+      <div v-if="!loading" v-for="post in posts" class="post">
+        <a :href="'/post/' + post.id" class="post__link">
+          <h2 class="post__title" v-html="post.title.rendered"/>
+        </a>
+
+        <p class="post__excerpt" v-html="post.excerpt.rendered"/>
+      </div>
+    </div>
+
+    <loader :show="loading"/>    
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import loader from '~/components/loader.vue'
+
+export default {
+  components: {
+    loader,
+  },
+  computed: {
+    variantsPostsPerPage() {
+      return this.$store.state.variantsPostsPerPage;
+    },
+    actualTotalPages() {
+      return this.$store.state.totalPages;
+    },
+    posts: function(){
+      this.loading = false;
+      return this.$store.state.postsArray;
+    },
+  },
+  methods: {
+    changeVariantPerPage(event) {
+      const allLinks = document.querySelectorAll('.list__link'),
+            content  = event.target.textContent.trim();
+
+      allLinks.forEach(link => {
+        if (link.classList.contains('list__link--active')) {
+          link.classList.remove('list__link--active');
+        }
+      });
+
+      event.target.classList.add('list__link--active');
+
+      this.loading = true;
+      this.$store.commit('setPostsPerPage', content);
+      this.$store.dispatch('actionGetPost');
+    },
+    setNextPage() {
+      this.loading = true;
+      this.$store.commit('nextPage');
+      this.$store.dispatch('actionGetPost');
+    },
+    setPrevPage() {
+      this.loading = true;
+      this.$store.commit('prevPage');
+      this.$store.dispatch('actionGetPost');
+    }
+  },
+  fetch({ store, params }) {
+    return axios.get(`http://poldon.pl/wp-json/wp/v2/posts?page=${store.state.page}&per_page=${store.state.postsPerPage}`)
+      .then((response) => {
+        const numberOfPages = Math.ceil(response.headers['x-wp-total'] / store.state.postsPerPage);
+
+        store.commit('addPosts', response.data);
+        store.commit('setTotalPages', numberOfPages);
+      });
+  },
+  data() {
+    return {
+      loading: true
+    }
+  },
+  beforeMount() {
+    return this.loading = true;
+  },
+  mounted() {
+    return this.loading = false;
+  },
+}
+</script>
