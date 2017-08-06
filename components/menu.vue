5e5332eb2a6dd822c0a5cde70e79dbed78f50f7f
Filip Kudła <filip.kudla@snow.dog>
2017-08-07 00:00:22 +0200

    Add menu in header and styles

---
+++ components/menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="header__menu list">
+    <li class="list__item">
+      <router-link to="/" class="list__link">
+        Posty
+      </router-link>
+    </li>
+
+    <li>
+      <router-link to="/recenzje" class="list__link">
+        Recenzje
+      </router-link>
+    </li>
+  </ul>
+</template>
--- components/toolbar.vue
+++ components/toolbar.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="container">
-    <router-link to="/">
-      <img src="logo.png" class="haeder__logo" />
-    </router-link>
-  </div>
+  <header class="header">
+    <div class="container">
+      <router-link to="/" class="header__link">
+        <img src="logo.png" class="header__logo" />
+      </router-link>
+
+      <menuList/>
+    </div>
+  </header>
 </template>
+
+<script>
+import menuList from '~/components/menu.vue'
+
+export default {
+  components: {
+    menuList
+  }
+}
+</script>
