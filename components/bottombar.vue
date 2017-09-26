3992b969537513ea96318b2994025a29f997be44
Filip Kudła <filip.kudla@snow.dog>
2017-09-27 00:25:33 +0200

    Fix styles, add error page base template and edit bottombar - add info

--- components/bottombar.vue
+++ components/bottombar.vue
@@ -1,9 +1,35 @@
 <template>
   <footer class="footer">
-    <div class="container footer__wrapper">
-      <span>
-        Created by FK
-      </span>
+    <div class="container">
+      <div class="footer__wrapper">
+        <p class="footer__info">
+          © 2017 POLSKI STREETWEAR | POLDON - INTERNETOWY MAGAZYN O POLSKIM STREETWEARZE
+        </p>
+        
+        <p class="footer__info footer__info--right">
+          Created by Filip Kudła
+
+          <a href="https://github.com/kjugi/" target="_blank" class="footer__link">
+            My Github
+          </a>
+        </p>
+      </div>
+
+      <div class="footer__column list">
+        <a href="https://www.facebook.com/poldonpl/" 
+           target="_blank" 
+           class="list__item"
+        >
+          facebook
+        </a>
+
+        <a href="https://www.instagram.com/poldon.pl/" 
+           target="_blank" 
+           class="list__item"
+        >
+          insta
+        </a>
+      </div>
     </div>
   </footer>
 </template>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,21 +1,50 @@
 <template>
-    <div class="container">
-        <h1 v-if="error.statusCode === 404">
-            Page not found - 404
-        </h1>
+  <transition>
+    <div>
+      <toolbar/>
 
-        <h1 v-else>
-            Wystąpił błąd
-        </h1>
+      <div class="container page">
+        <div class="page__handler">
+          <h1 class="heading" v-if="error.statusCode === 404">
+              Page not found - 404
+          </h1>
 
-        <nuxt-link to="/">
-            Homepage
-        </nuxt-link>
+          <h1 class="heading" v-else>
+              Wystąpił błąd
+          </h1>
+        </div>
+
+          <nuxt-link v-if="error.statusCode === 404" class="page__error" to="/">
+            Jedyna droga do wyboru teraz, to powrót do strony głównej.
+          </nuxt-link>
+
+          <p v-else class="page__error">
+            To juz jest koniec.
+          </p>
+      </div>
+
+      <bottombar/>
+      <loader/>
     </div>
+  </transition>
 </template>
 
 <script>
+import toolbar from '~/components/toolbar.vue'
+import bottombar from '~/components/bottombar.vue'
+import loader from '~/components/loader.vue'
+
 export default {
-  props: ['error'],
+  props: [
+    'error'
+  ],
+  components: {
+    bottombar,
+    toolbar,
+    loader
+  },
+  mounted() {
+    return this.$store.state.loading = false;
+  }
 }
 </script>
