960e5d2c98eb03eb2b25cdacc50c1f30a476f01a
Filip Kudła <filip.kudla@snow.dog>
2018-01-09 00:51:49 +0100

    Repair error layout with following styleguide

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,29 +1,23 @@
 <template>
   <transition>
-    <div>
-      <BaseToolbar></BaseToolbar>
+    <div class="container page">
+      <div class="page__handler">
+        <h1 class="heading" v-if="error.statusCode === 404">
+            Page not found - 404
+        </h1>
 
-      <div class="container page">
-        <div class="page__handler">
-          <h1 class="heading" v-if="error.statusCode === 404">
-              Page not found - 404
-          </h1>
-
-          <h1 class="heading" v-else>
-              Wystąpił błąd
-          </h1>
-        </div>
-
-        <nuxt-link v-if="error.statusCode === 404" class="page__error" to="/">
-          Jedyna droga do wyboru teraz, to powrót do strony głównej.
-        </nuxt-link>
-
-        <p v-else class="page__error">
-          To juz jest koniec.
-        </p>
+        <h1 class="heading" v-else>
+            Wystąpił błąd
+        </h1>
       </div>
 
-      <BaseBottomBar></BaseBottomBar>
+      <nuxt-link v-if="error.statusCode === 404" class="page__error" to="/">
+        Jedyna droga do wyboru teraz, to powrót do strony głównej.
+      </nuxt-link>
+
+      <p v-else class="page__error">
+        To juz jest koniec.
+      </p>
     </div>
   </transition>
 </template>
@@ -33,9 +27,11 @@ import BaseToolbar from '~/components/BaseToolbar.vue'
 import BaseBottomBar from '~/components/BaseBottomBar.vue'
 
 export default {
-  props: [
-    'error'
-  ],
+  props: {
+    error: {
+      type: Object
+    }
+  },
   components: {
     BaseBottomBar,
     BaseToolbar
